070af11dee77d83d2a2130a21a08a69231bc9a0f
JuneMbuvi <mbuvimbatha@gmail.com>
2024-04-04 03:51:40 -0700

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""method that determines if a given
+data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """Returns True if data is a valid UTF-8 encoding, else return False"""
+    i = 0
+    while i < len(data):
+        leading_ones = 0
+        mask = 1 << 7
+        while mask and data[i]:
+            leading_ones += 1
+            mask >>= 1
+        if leading_ones == 0:
+            i += 1
+            continue
+        if leading_ones == 1 or leading_ones > 4:
+            return False
+        trailing_bytes = leading_ones - 1
+        i += 1
+        while trailing_bytes > 0:
+            if i >= len(data) or (data[i] >> 6) != 0b10:
+                return False
+            i += 1
+            trailing_Bytes -= 1
+    return True
